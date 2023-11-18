80e9197608f5425015f221b5e102ab8494c9d6e5
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2023-11-18 02:02:17 +0100

    small changes in  homepage.css

--- client/internship/src/components/Homepage.jsx
+++ client/internship/src/components/Homepage.jsx
@@ -12,28 +12,30 @@ const HomePage = () => {
         Discover a seamless shopping experience with Shopping Time. Browse our list of shoppers,
         explore a variety of items, and create personalized shopping lists.
       </p>
-      <div className="get-started-section">
-        <h2>Get Started</h2>
-        <ul>
-          <li>
-            <Link to="/create-list">Create a Shopping List</Link>
-          </li>
-          <li>
-            <Link to="/all-lists">View All Shopping Lists</Link>
-          </li>
-        </ul>
-      </div>
-      <div className="explore-section">
-        <h2>Explore</h2>
-        <ul>
-          <li>
-            <Link to="/items">Browse Items</Link>
-          </li>
-          <li>
-            <Link to="/shoppers">Explore Shoppers</Link>
-          </li>
-        </ul>
-      </div>
+     
+<div className="get-started-section">
+  <h2>Get Started</h2>
+  <ul>
+    <li>
+      <Link to="/create-list">Create a Shopping List</Link>
+    </li>
+    <li>
+      <Link to="/all-lists">View All Shopping Lists</Link>
+    </li>
+  </ul>
+</div>
+<div className="explore-section">
+  <h2>Explore</h2>
+  <ul>
+    <li>
+      <Link to="/items">Browse Items</Link>
+    </li>
+    <li>
+      <Link to="/shoppers">Explore Shoppers</Link>
+    </li>
+  </ul>
+</div>
+
     </div>
   );
 };
